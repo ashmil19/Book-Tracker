0d0a85cbd65e56a815093ed1866a03afa7994d71
Ashmil Abdul Rasheed <ashmilabdulrasheed@gmail.com>
2022-12-15 15:48:54 +0530

    android backbutton event modified in update screen

--- lib/presentation/library/widgets/update_book.dart
+++ lib/presentation/library/widgets/update_book.dart
@@ -25,78 +25,93 @@ class UpdateBook extends StatelessWidget {
         ..add(bookNameChangedEvent(changedBookName: bookModel.bookName))
         ..add(AuthorNameChangedEvent(changedAuthorName: bookModel.authorName!));
     });
-    return Scaffold(
-      backgroundColor: kWhiteColor,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormFieldWidget(
-                labelText: "Book Name",
-                onChanged: (book) => context
-                    .read<LibraryBloc>()
-                    .add(bookNameChangedEvent(changedBookName: book.trim())),
-                textEditingController: _bookNameController,
-              ),
-              kHeight20,
-              TextFormFieldWidget(
-                labelText: "Author Name",
-                onChanged: (author) => context.read<LibraryBloc>().add(
-                    AuthorNameChangedEvent(changedAuthorName: author.trim())),
-                textEditingController: _authorNameController,
-              ),
-              kHeight20,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  BlocBuilder<LibraryBloc, LibraryState>(
-                    builder: (context, state) {
-                      return LibraryButtonWidget(
-                        text: "Update",
-                        onPressed: !state.updateBookNameEmpty &&
-                                !(state.changedBookName == bookModel.bookName &&
-                                    state.changedAuthorName ==
-                                        bookModel.authorName)
-                            ? () {
-                                final _bookName =
-                                    _bookNameController.text.trim();
-                                final _authorName =
-                                    _authorNameController.text.trim();
+    return WillPopScope(
+      onWillPop: () {
+        final value = _onWillPop(context);
+        return value;
+      },
+      child: Scaffold(
+        backgroundColor: kWhiteColor,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormFieldWidget(
+                  labelText: "Book Name",
+                  onChanged: (book) => context
+                      .read<LibraryBloc>()
+                      .add(bookNameChangedEvent(changedBookName: book.trim())),
+                  textEditingController: _bookNameController,
+                ),
+                kHeight20,
+                TextFormFieldWidget(
+                  labelText: "Author Name",
+                  onChanged: (author) => context.read<LibraryBloc>().add(
+                      AuthorNameChangedEvent(changedAuthorName: author.trim())),
+                  textEditingController: _authorNameController,
+                ),
+                kHeight20,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    BlocBuilder<LibraryBloc, LibraryState>(
+                      builder: (context, state) {
+                        return LibraryButtonWidget(
+                          text: "Update",
+                          onPressed: !state.updateBookNameEmpty &&
+                                  !(state.changedBookName ==
+                                          bookModel.bookName &&
+                                      state.changedAuthorName ==
+                                          bookModel.authorName)
+                              ? () {
+                                  final _bookName =
+                                      _bookNameController.text.trim();
+                                  final _authorName =
+                                      _authorNameController.text.trim();
 
-                                context.read<LibraryBloc>().add(UpdateBookEvent(
-                                      id: bookModel.id!,
-                                      updateBookName: _bookName,
-                                      updateAuthorName: _authorName,
-                                      bookStatus: bookModel.bookStatus,
-                                    ));
+                                  context
+                                      .read<LibraryBloc>()
+                                      .add(UpdateBookEvent(
+                                        id: bookModel.id!,
+                                        updateBookName: _bookName,
+                                        updateAuthorName: _authorName,
+                                        bookStatus: bookModel.bookStatus,
+                                      ));
 
-                                context
-                                    .read<LibraryBloc>()
-                                    .add(const GetAllBooksEvent());
+                                  context
+                                      .read<LibraryBloc>()
+                                      .add(const GetAllBooksEvent());
 
-                                int count = 0;
-                                Navigator.of(context)
-                                    .popUntil((_) => count++ >= 2);
-                              }
-                            : null,
-                      );
-                    },
-                  ),
-                  LibraryButtonWidget(
-                    text: "Back",
-                    onPressed: () {
-                      int count = 0;
-                      Navigator.of(context).popUntil((_) => count++ >= 2);
-                    },
-                  ),
-                ],
-              )
-            ],
+                                  int count = 0;
+                                  Navigator.of(context)
+                                      .popUntil((_) => count++ >= 2);
+                                }
+                              : null,
+                        );
+                      },
+                    ),
+                    LibraryButtonWidget(
+                      text: "Back",
+                      onPressed: () {
+                        int count = 0;
+                        Navigator.of(context).popUntil((_) => count++ >= 2);
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Future<bool> _onWillPop(context) {
+    int count = 0;
+    Navigator.of(context).popUntil((_) => count++ >= 2);
+    return Future.value(true);
   }
 }
